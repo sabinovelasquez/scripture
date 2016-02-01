fbd47e16a3b3fe92fd1c87d71501fd71f872233d
Sabino Velasquez <sabino@Sabinos-MacBook-Pro.local>
2016-02-01 20:41:10 -0300

    css - initial load

---
+++ source/js/all.js
@@ -0,0 +1,11 @@
+//= require underscore
+
+//=require vendor/angular/angular.min
+//=require vendor/angular/angular-animate
+//=require vendor/angular/angular-ui-bootstrap-modal
+//=require vendor/angular/angular-parallax
+//=require vendor/angular/angular-scroll.min
+//=require vendor/ui-bootstrap-tpls-0.14.3.min
+//=require vendor/angulartics
+//=require vendor/angulartics-google-analytics
+//= require_tree ./lib
