d27985596d122f7842b34e9927a4239dee978fd4
Sabino Velasquez <sabino@Sabinos-MacBook-Pro.local>
2016-02-12 20:46:51 -0300

    structure

--- src/scripture/scripture/urls.py
+++ src/scripture/scripture/urls.py
@@ -27,7 +27,7 @@ router.register(r'scripts', MovieViewSet)
 
 urlpatterns = [
     url(r'^', include('movies.urls')),
-    url(r'^api', include(router.urls)),
+    url(r'^rest/api', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^script/admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
